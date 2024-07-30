49e825104ae8cb85c593a010805d0e0a21a09df2
Vsevolod Pletnev <vseplet@gmail.com>
2024-07-30 15:09:09 +0700

    update fecthify
    hmmm

--- source/mod.ts
+++ source/mod.ts
@@ -1,7 +1,7 @@
 import type { ApiflyGuards, ApiflyPatch, ApiflyWatchers } from "$types";
 import fetchify from "@vseplet/fetchify";
-// import { Fetchify } from "https://jsr.io/@vseplet/fetchify/0.3.17/source/fetchify.ts";
-// import { IFetchifyConfig } from "https://jsr.io/@vseplet/fetchify/0.3.17/source/types.ts";
+import type { Fetchify } from "@vseplet/fetchify/Fetchify";
+import type { IFetchifyConfig } from "@vseplet/fetchify/types";
 
 export class ApiflyServer<T> {
   constructor(private state: T) {
@@ -21,24 +21,24 @@ export class ApiflyServer<T> {
 }
 
 export class ApiflyClient<T> {
-  private fetchify: {}; // late
+  private fetchify: Fetchify; // late
 
-  constructor(private state: T, fetchifyConfig?: {}) {
+  constructor(private state: T, fetchifyConfig?: IFetchifyConfig) {
     this.fetchify = fetchify.create(fetchifyConfig);
   }
 
   async get(): Promise<Partial<T>> {
     throw new Error("Not implemented");
-    // this.fetchify.get("/state");
+    this.fetchify.get("/state");
   }
 
   async patch(patch: ApiflyPatch<T>) {
     throw new Error("Not implemented");
-    // this.fetchify.patch("/state", patch);
+    this.fetchify.patch("/state", patch);
   }
 
   async call(method: string, args: any[]) {
     throw new Error("Not implemented");
-    // this.fetchify.post("/call");
+    this.fetchify.post("/call");
   }
 }
