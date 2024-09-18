7c84608ee007d5218551f202de0cf1cd29a56bda
Artem <artp80@gmail.com>
2024-09-18 18:33:12 +0700

    ПРОТОЛКНУТЬ ПУШ(обновил докстирнги в клиентеъ)

--- source/ApiflyClient.ts
+++ source/ApiflyClient.ts
@@ -12,19 +12,38 @@ import fetchify from "@vseplet/fetchify";
 import type { Fetchify } from "@vseplet/fetchify/Fetchify";
 import type { IFetchifyConfig } from "@vseplet/fetchify/types";
 
+/**
+ * ApiflyClient class is responsible for handling client-side requests to
+ * interact with an Apifly server. It provides methods to get the current
+ * state, patch it, or call server-side procedures via RPC.
+ */
 export class ApiflyClient<D extends ApiflyDefinition<any, any>> {
   private fetchify: Fetchify;
 
+  /**
+   * Constructor to initialize the ApiflyClient.
+   * @param {IFetchifyConfig} [fetchifyConfig] - Optional configuration for the fetchify client.
+   */
   constructor(fetchifyConfig?: IFetchifyConfig) {
     this.fetchify = fetchify.create(fetchifyConfig);
   }
 
+  /**
+   * Sends a request to the server using fetchify.
+   * @param {any} body - The request body to be sent to the server.
+   * @returns {Promise<Response>} - The server's response.
+   * @private
+   */
   private async sendRequest(body: any): Promise<Response> {
     return await this.fetchify.post("", {
       body: JSON.stringify(body),
     });
   }
 
+  /**
+   * Sends a "get" request to the server to retrieve the current state.
+   * @returns {Promise<[ApiflyResponse<InferStateType<D>> | null, Error | null]>} - Returns the state or an error.
+   */
   async get(): Promise<
     [ApiflyResponse<InferStateType<D>> | null, Error | null]
   > {
@@ -41,6 +60,12 @@ export class ApiflyClient<D extends ApiflyDefinition<any, any>> {
     }
   }
 
+  /**
+   * Sends a "patch" request to the server to modify the current state.
+   * @param {ApiflyPatch<InferStateType<D>>} patch - The patch object that contains changes to be applied.
+   * @returns {Promise<ApiflyResponse<InferStateType<D>>>} - The modified state after applying the patch.
+   * @throws {Error} - Throws an error if the patch request fails.
+   */
   async patch(
     patch: ApiflyPatch<InferStateType<D>>,
   ): Promise<ApiflyResponse<InferStateType<D>>> {
@@ -59,6 +84,13 @@ export class ApiflyClient<D extends ApiflyDefinition<any, any>> {
     }
   }
 
+  /**
+   * Sends a "call" request to the server to invoke a remote procedure.
+   * @param {keyof D["rpc"]} name - The name of the procedure to be invoked.
+   * @param {InferRpcListArgs<D["rpc"]>[N]} args - The arguments to pass to the procedure.
+   * @returns {Promise<InferRpcListReturns<D["rpc"]>[N]>} - The result returned by the procedure.
+   * @throws {Error} - Throws an error if the call request fails.
+   */
   async call<N extends keyof D["rpc"]>(
     name: N,
     args: InferRpcListArgs<D["rpc"]>[N],
